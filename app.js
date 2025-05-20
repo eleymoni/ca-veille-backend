08aa7184fd6b60fdd9a5449b170b99c4396224a3
michael <maroc.michael@hotmail.com>
2025-05-20 16:25:41 +0200

    update app.js

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-require('dotenv').config();
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require('cors');
+require("dotenv").config();
+require("./config/connection");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const authRouter = require("./routes/auth");
+const { errorHandler } = require("./middlewares/errorHandler");
 
 var app = express();
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/auth", authRouter);
+app.use(errorHandler);
 
 module.exports = app;
