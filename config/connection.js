de04ddbf86cdaa146b9ffed49908a241dd56fde6
michael <maroc.michael@hotmail.com>
2025-05-20 10:50:13 +0200

    add project starter

---
+++ config/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const connectionString = process.env.CONNECTION_STRING;
+
+mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log('🔥 Database connected 🔥'))
+  .catch(error => console.error(error));
