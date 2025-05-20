462e1c7448910881af522344fae036a23f412ff3
ThomasMoulins <13thomasmoulins@gmail.com>
2025-05-20 18:14:40 +0200

    change feed route

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const cors = require("cors");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
+const feedsRouter = require("./routes/feeds");
 const { errorHandler } = require("./middlewares/errorHandler");
-var feedsRouter = require("./routes/feeds");
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
-app.use(errorHandler);
 app.use("/feeds", feedsRouter);
+app.use(errorHandler);
 
 module.exports = app;
