0dc24c6c1b83ba580368611fbfb14157417384ec
ThomasMoulins <13thomasmoulins@gmail.com>
2025-05-22 01:12:10 +0200

    Add name to model feeds

--- models/feeds.js
+++ models/feeds.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const feedsSchema = mongoose.Schema({
     url: { type: String, require: true },
+    name: { type: String, require: true },
     articles: [{ type: mongoose.Types.ObjectId, ref: "articles" }],
     defaultMedia: { type: String, default: null },
 });
