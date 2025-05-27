1c8508c6e319379ca3d9fb72856383c94b8d5589
ThomasMoulins <13thomasmoulins@gmail.com>
2025-05-27 17:26:02 +0200

    Add sort by date + limit to 50 articles

--- controllers/feeds.controller.js
+++ controllers/feeds.controller.js
@@ -35,6 +35,16 @@ const addFeedToBdd = async (siteUrl, categoryId, res) => {
         const items = result?.feed?.entry || result?.rss?.channel?.item;
         const logo = result?.feed?.logo || result?.rss?.channel?.image?.url;
 
+        // Étape 4 : Tri du plus récent au plus ancien
+        items.sort((a, b) => {
+            const dateA = new Date(a.updated || a.pubDate);
+            const dateB = new Date(b.updated || b.pubDate);
+            return dateB - dateA;
+        });
+
+        // Étape 5 : Limite à 50 articles
+        items = items.slice(0, 50);
+
         const articleArray = await Promise.all(
             items.map(async (item) => {
                 const newArticle = new ArticleModel({
@@ -52,6 +62,7 @@ const addFeedToBdd = async (siteUrl, categoryId, res) => {
                         item["media:content"]?.$?.url ||
                         item.enclosure?.$?.url ||
                         item.enclosure?.url ||
+                        item.image ||
                         null,
                     date: item.updated || item.pubDate,
                     author:
